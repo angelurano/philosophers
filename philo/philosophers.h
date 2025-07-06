d523417d54869501c6df4dd72af6a1563f4efdc4
Miguel Angel Garcia Beltran <angelurano00@gmail.com>
2025-07-06 21:33:19 +0200

    First commit

---
+++ philo/philosophers.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: migugar2 <migugar2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/06 18:44:38 by migugar2          #+#    #+#             */
+/*   Updated: 2025/07/06 21:13:58 by migugar2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h> // memset
+# include <stdio.h> // printf
+# include <stdlib.h> // malloc, free
+# include <unistd.h> // write, usleep
+# include <sys/time.h> // gettimeofday
+# include <pthread.h>
+// pthread_create, pthread_detach, pthread_join
+// pthread_mutex_init, pthread_mutex_destroy
+// pthread_mutex_lock, pthread_mutex_unlock
+// pthread_mutex_t
+
+# include <sys/types.h> // usecondst_t
+
+typedef pthread_mutex_t	t_fork;
+
+#endif
